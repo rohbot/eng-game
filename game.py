3a02000cfe45485759e90c721c1f4a8fd6cecf5e
rohan <rohan@biji-biji.com>
2017-07-02 22:10:10 +0800

    changes for screens

--- game.py
+++ game.py
@@ -6,7 +6,6 @@ import sys
 strPort1 = '/dev/ttyACM0'
 strPort2 = '/dev/ttyACM1'
 
-ser = serial.Serial(strPort1, 9600)
 
 
 
@@ -21,59 +20,64 @@ R_WIDTH = L_WIDTH + (WIDTH / 2)
 
 images = []
 
+
 path = "/home/pi/eng-game/assets/"
 #path = "/home/roh/biji-biji/pintar/eng-game/assets/"
 
-
+# Show logo
 BASE_CMD = "sudo fbi -T 1 --noverbose -a "
 #path = "assets/"
+cmd = BASE_CMD + path + "bb.jpg"
+os.system(cmd)
 
 
-global counting, start_time, r_counter, l_counter
+ser = serial.Serial(strPort1, 9600)
+
+
+global counting, start_time, r_counter, l_counter, screen
 
 for i in range(10):
 	fn = path + 'ENG0' + str(i) + '.png'
 	images.append(pygame.image.load(fn))
 
-img = {'start':"ENGRP.jpg",
-		'ready':"ENGGR.jpg",
-		'1':"ENG01.jpg",
-		'2':"ENG02.jpg",
-		'3':"ENG03.jpg",
-		'cycle': "ENGCY.jpg",
-		'logo':"bb.jpg",
-		'game-bg': "ENGBG.jpg",
-		'ms': "ENGMS.jpg",
-		'cg': "ENGCG.jpg",
-		'ta': "ENGTA.jpg",
-		'sc': "ENGSC.jpg",
+
+img = {'start':pygame.image.load(path + "ENGST.jpg"),
+		'ready':pygame.image.load(path + "ENGGR.jpg"),
+		'finish':pygame.image.load(path + "ENGFN.jpg"),
+		'logo':pygame.image.load(path + "bb.jpg"),
+		'game-bg': pygame.transform.scale(pygame.image.load(path + "ENGBG.jpg"),(WIDTH,HEIGHT)),
+		'wait': pygame.image.load(path + "ENGRP.jpg"),
+		'success': pygame.image.load(path + "ENGCG.jpg"),
+		'tryagain': pygame.image.load(path + "ENGTA.jpg"),
+		'code': pygame.image.load(path + "ENGSC.jpg"),
 		}
 
-img_start = pygame.image.load(path + "ENGRP.jpg")
-img_start = pygame.transform.scale(img_start,(1280,720))
 
-img_ready = pygame.image.load(path + "ENGGR.jpg")
-img_ready = pygame.transform.scale(img_ready,(1280,720))
+# img_start = pygame.image.load(path + "ENGRP.jpg")
+# img_start = pygame.transform.scale(img_start,(1280,720))
 
-img_logo = pygame.image.load(path + "bb.jpg")
-img_logo = pygame.transform.scale(img_logo,(1280,720))
+# img_ready = pygame.image.load(path + "ENGGR.jpg")
+# img_ready = pygame.transform.scale(img_ready,(1280,720))
 
-
-pygame.init()
+# img_logo = pygame.image.load(path + "bb.jpg")
+# img_logo = pygame.transform.scale(img_logo,(1280,720))
 
 
 
-screen = pygame.display.set_mode((WIDTH, HEIGHT), pygame.FULLSCREEN)
-done = False
-clock = pygame.time.Clock()
-
-screen.fill((255, 255, 255))
+#screen.fill((255, 255, 255))
 
 counting = False
 won = False
 
+def show_image(imag):
+	global screen
+	screen.fill((255, 255, 255))
+	screen.blit(img[imag], (0, 0))
+	pygame.display.flip()
+
 
 def start_counter(duration):
+	global screen
 	r_counter = duration % 10
 	duration /= 10
 	l_counter = duration % 10
@@ -85,25 +89,24 @@ def start_counter(duration):
 	start_time = time.time()
 
 def waiting():
-	screen.fill((255, 255, 255))
-	screen.blit(img_start, (0, 0))
-	pygame.display.flip()
-
+	#print 'Waiting'
+	show_image('wait')
 
 def showReady(line):
-	if line[2] == "R": 
-		show_image('2')
+	global screen
+	if line[2] == "R":
+		print 'show ready' 
+		show_image('ready')
+
+	if line[2] == "S":
+		print 'show start' 
+		show_image('start')
 		# screen.fill((255, 255, 255))
 		# screen.blit(images[2], (0, 0))
 		# pygame.display.flip()
-	elif line[2] == "S": 
-		show_image('1')
+	elif line[2] == "F": 
+		show_image('finish')
 		
-		# screen.fill((255, 255, 255))
-		# screen.blit(images[1], (0, 0))
-		# pygame.display.flip()
-	elif line[2] == "G": 
-		show_image('0')
 		# screen.fill((255, 255, 255))
 		# screen.blit(images[1], (0, 0))
 		# pygame.display.flip()
@@ -122,32 +125,41 @@ def showReady(line):
 		screen.blit(l_image, (L_WIDTH, 50))
 		screen.blit(r_image, (R_WIDTH, 50))
 		pygame.display.flip()
-	
+	#print line
 
 
 def showResult(line):
-	if line[2] == "C": 
-		screen.fill((255, 255, 255))
-		screen.blit(img_ready, (0, 0))
-		pygame.display.flip()
+	if line[2] == "S":
+		show_image('success') 
+		# screen.fill((255, 255, 255))
+		# screen.blit(img_ready, (0, 0))
+		# pygame.display.flip()
+
+	if line[2] == "T":
+		print 'Trying again'
+		show_image('tryagain') 
+		# screen.fill((255, 255, 255))
+		# screen.blit(img_ready, (0, 0))
+		# pygame.display.flip()
+	if line[2] == "C":
+		show_image('code') 
+		
 	
-def show_image(imag):
-	cmd = BASE_CMD + path + img[imag]
-	os.system(cmd)
 
 def showLogo():
 	screen.fill((255, 255, 255))
 	screen.blit(img_logo, (0, 0))
 	pygame.display.flip()
 
-
+\
 
 #showLogo()
-			
 #waiting()
-
+pygame.init()
+screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
 show_image('logo')
-waiting()
+#
+#waiting()
 while 1:
 	try:
 		line = ser.readline().strip()
@@ -162,7 +174,7 @@ while 1:
 		if line[0] == 'C':
 			showReady(line)	
 
-		if line[0] == 'D':
+		if line[0] == 'P':
 			showResult(line)	
 
 	except KeyboardInterrupt:
